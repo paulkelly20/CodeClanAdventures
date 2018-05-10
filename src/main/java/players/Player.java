a8d75f3a5598ecbbe9217476996a9276569d7bfd
Daniel Childs <dc.1994@outlook.com>
2018-05-10 15:24:10 +0100

    create treasure and add treasure to players

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -2,21 +2,29 @@ package players;
 
 import interfaces.takeDamagable;
 import item.Healingtool;
+import item.Treasure;
+
+import java.util.ArrayList;
 
 public abstract class Player implements takeDamagable{
 
     private String name;
     protected int healthPoints;
+    private ArrayList<Treasure> treasure;
 
     public Player(String name, int healthPoints) {
         this.name = name;
         this.healthPoints = healthPoints;
+        this.treasure = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
+    public int getTreasure() {
+        return treasure.size();
+    }
 
     public int getHealthPoints() {
         return healthPoints;
@@ -26,4 +34,7 @@ public abstract class Player implements takeDamagable{
         this.healthPoints -= damage;
     }
 
+    public void addTreasure(Treasure treasure){
+        this.treasure.add(treasure);
+    }
 }
